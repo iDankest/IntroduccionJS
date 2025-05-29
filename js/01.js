a9684f0d25b115933717f8c1d314bacf55a5dcf3
Kilian <dankestmirror@hotmail.es>
2025-05-29 19:13:31 +0100

    Uso de Const y let

--- js/01.js
+++ js/01.js
@@ -1,3 +1,12 @@
 //Variables 
+//la palabra reservada let es para declarar variables scope local 
+//la palabra reservada var es para declarar variables scope global desuso
+//la palabra reservada const es para declarar variables constantes
 
 let cliente = "Juan";
+cliente = "Pedro"; // la variable en JS se puede reasignar
+console.log(cliente);
+
+const precio = 200;
+precio = 300; // la variable en JS no se puede reasignar
+console.log(precio);
---
+++ js/02.js
@@ -0,0 +1,6 @@
+// ejemplo de Const y su uso
+const precio = 200;
+precio = 300; // la variable en JS no se puede reasignar
+console.log(precio);
+//Esta variable no cambia su valor en ejecucion
+// Tiene mayor seguridad que let y se recomienda usar const por defecto
