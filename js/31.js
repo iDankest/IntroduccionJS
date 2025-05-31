d28ff9da13691765f44cfd156bf28e16b8803bed
Kilian <dankestmirror@hotmail.es>
2025-05-31 19:12:04 +0100

    async

--- js/31.js
+++ js/31.js
@@ -1,1 +1,16 @@
 const url = "https://jsonplaceholder.typicode.com/comments"
+//Async/Await try/catch
+
+const consultarAPI = async () => {//El async es obligatorio para que funcione y se pude declarar al inicio de la funcion async function consultarAPI(){
+    try {
+        const respuesta = await fetch(url);//el fetch es la peticion
+        const resultado = await respuesta.json();//el json es la respuesta
+        console.log(resultado);
+    } catch (error) {//El catch es obligatorio y te dice que paso algo
+        console.log(error);
+    } finally{//El finally no es obligatorio y se ejecuta siempre
+        console.log("todo listo")
+    }
+}
+consultarAPI();
+//Esta es la sintaxis mas moderna y para mi la entiendo mejor que el fetch
