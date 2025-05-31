56bfd880bad657edd77c942ef6f977f42c4458fc
Kilian <dankestmirror@hotmail.es>
2025-05-31 19:02:50 +0100

    Llamada a una API fetch

--- js/30.js
+++ js/30.js
@@ -1,1 +1,19 @@
 const url = "https://jsonplaceholder.typicode.com/comments"
+//Fetch API and promises
+
+const consultarAPI = () => {
+    fetch(url)
+    .then((respuesta)=>{
+        return respuesta.json()
+    })
+    .then(resultado=>{
+        console.log(resultado)
+    })
+    .catch(error=>{
+        console.log(error)
+    })
+    .finally(()=>{
+        console.log("Finalizado")
+    })     
+}
+consultarAPI();
